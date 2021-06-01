dbed25a48a53429fea491dbf1705ee2c4ebd895c
yunu <oa8859@gmail.com>
2021-06-01 18:01:14 +0900

    practice04

--- src/com/javaex/practice/Ex07.java
+++ src/com/javaex/practice/Ex07.java
@@ -5,7 +5,9 @@ import java.util.Scanner;
 public class Ex07 {
 
 	public static void main(String[] args) {
-		int[] wonArray = new int[10];
+		int[] wonArray = {50000, 10000, 5000, 1000, 500, 100, 50, 10, 5, 1};
+		
+		/*
 		wonArray[0] = 50000;
 		wonArray[1] = 10000;
 		wonArray[2] = 5000;
@@ -16,6 +18,7 @@ public class Ex07 {
 		wonArray[7] = 10;
 		wonArray[8] = 5;
 		wonArray[9] = 1;
+		*/
 		
 		Scanner sc=new Scanner(System.in);
 		
--- src/com/javaex/practice/Ex08.java
+++ src/com/javaex/practice/Ex08.java
@@ -6,9 +6,17 @@ public class Ex08 {
 		int [] lotto = new int[6];
 		
 		for(int i=0; i<lotto.length; i++) {
-			lotto[i] = (int)(Math.random()*45)+1;
-			System.out.print(lotto[i]+"\t");
+			int num = (int)(Math.random()*45)+1;
+			for(int j = 0; j<lotto.length; j++) {
+				if(num == lotto[j]) {
+					//System.out.print("num은" + num + "\t"); //중복test
+					num = (int)(Math.random()*45)+1;
+				}
+			}
+			lotto[i] = num;
+			System.out.print(lotto[i] + "\t");
 		}
+		
 
 	}
 
