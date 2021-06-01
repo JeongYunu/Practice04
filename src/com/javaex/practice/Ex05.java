efeed5a5f3b0acb58f0085a0541b8ec1e821f9ab
yunu <oa8859@gmail.com>
2021-06-01 09:50:45 +0900

    practice04

--- src/com/javaex/practice/Ex05.java
+++ src/com/javaex/practice/Ex05.java
@@ -1,9 +1,29 @@
 package com.javaex.practice;
 
+import java.util.Scanner;
+
 public class Ex05 {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+		Scanner sc=new Scanner(System.in);
+		
+		int[] data = new int[5];
+		
+		int sum = 0;
+		
+		for(int i = 0; i<data.length; i++) {
+			System.out.print("");
+			data[i] = sc.nextInt();
+			sum += data[i];
+		}
+		
+		System.out.println("평균은 " + (sum/data.length) + " 입니다.");
+		
+		System.out.println("====================");
+		System.out.println(sum);
+		System.out.println(data.length);
+		
+		sc.close();
 
 	}
 
